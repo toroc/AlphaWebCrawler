9816361e15dc01e146a844a321f49efdd67826e2
Carol D. Toro <toroc@oregonstate.edu>
2016-06-28 17:51:51 -0700

    Modified link_visitor function to instatiate crawler objects, call corresponding traversal, and print the list of pages crawled. Also added the dfs_crawl function to carry out the crawl and update the crawler object it receives

--- link_visitor.py
+++ link_visitor.py
@@ -5,12 +5,12 @@ import time
 import pprint
 import logging
 random.seed(time.time())
-urls = [
-        'http://www.stackoverflow.com/',
+
+
+urls = ['http://www.stackoverflow.com/',
         'http://stackoverflow.com/',
         'http://google.com/',
-        'http://bing.com'
-    ]
+        'http://bing.com']
 
 def html_parser(url, keyword=None):
 
@@ -23,69 +23,83 @@ def rand_visiting(urls):
 
     rand_url = urls[rand_idx]
     #rand_url = rand_obj['url']
-    logging.warn(rand_url)
+    #logging.warn(rand_url)
 
     return rand_url
 
 def link_visitor(url, keyword=None, limit=10, DFS=True):
     """"""
-    page_urls = list()
-    page_list = list()
-    visited_urls = set()
-    url_q = queue.Queue()
-    url_q.put(url)
+    #Set uo crawl class object
+    crawl = crawl_class(url,limit, keyword)
+    
 
 
-    # Add starting URL to queue
-    title, page_urls, kwd_found = html_parser(url, keyword)
-    start_page = web_page(url, title, None)
-    #url_q.put(start_page['url'])
-    url_q.put(start_page.url)
+    #Set up the start page
+    start_page = visit(url,None, crawl.options)
 
-    while not url_q.empty() and limit >0:
+    #Add Start page to QUE
 
-        url = url_q.get()
+    crawl.url_q.put(start_page.url)
+    crawl.visited_set.add(start_page.url)
+    crawl.pages.visited.append(start_page.asDict)
+    if DFS:
+        dfs_crawl(crawl, start_page)
 
-        if url not in visited_urls:
+    print(crawl.pages.visited)
+
+
+
+def dfs_crawl(crawling, start_page):
+    neighbors = start_page.neighbors
+    from_page = start_page.url
+    
+    
+    while not crawling.url_q.empty() and crawling.options.limit > 0:
+        url = crawling.url_q.get()
+
+        next_url = rand_visiting(neighbors)
+
+        if next_url not in crawling.visited_set:
+            crawling.url_q.put(next_url)           
+        
+
+        if url not in crawling.visited_set:
             # Get page title and urls on page
-            title,page_urls, kwd_found = html_parser(url, keyword)
-            if kwd_found:
+            cur_page = visit(url, from_page, crawling.options)
+            from_page = cur_page.url
+            neighbors = cur_page.neighbors
+            crawling.visited_set.add(url)
+            crawling.pages.visited.append(cur_page.asDict)
+            
+            crawling.options.limit -=1
+
+            if crawling.options.kwd_found:
                 # Done
-                pass
-            visited_urls.add(url)    
-            cur_page = web_page(url, title, None)
-            page_list.append(cur_page.asDict)
-        # Call DFS/BFS based on input
-            #print(page_urls)
-            next_url = rand_visiting(page_urls)
-            url_q.put(next_url)
-            limit -=1
+                break
+       
+        
 
-    print(page_list)
 
-    """page_list contains visited URLS"""
-    # Return results
+    print("Done with the crawl")
+    logging.warn("Done with dfs crawl")
+   
 
-    # Create page objects for the URL only when visited
-    # for x in page_urls:
-    #     logging.warn(x)
-    #     # Set the page url and parent
-    #     temp_page = web_page(x, None, url)
-    #     logging.warn(temp_page)
-    #     # Convert to dictionary
-    #     page_list.append(temp_page.__dict__)
-    #     url_q.put(temp_page.__dict__)
 
-    # logging.warn(page_list)
-    # logging.warn(url_q)
-    # pp = pprint.PrettyPrinter(indent=4)
-    # pprint.saferepr(page_list)
+def bfs_crawl(options, start_page, visited_list, url_queue):
 
-    #url_q = queue.Queue()
-    
+    #add all neighbors to the the queue
+    neighbors = start_page.neighbors
+    for x in neighbors: 
+        url_queue.put(x)
 
-    
+    #while there are unvisited neighbors
+def visit(url, from_page=None, options=None):
 
+     title, page_urls, kwd_found = html_parser(url, options.keyword)
+     cur_page = web_page(url, title, from_page, page_urls)
+     options.kwd_found = kwd_found
+     
+     return cur_page
 
 link_visitor('test')
 
