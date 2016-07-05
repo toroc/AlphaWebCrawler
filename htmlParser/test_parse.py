3e75ebc6311ba7703ddd89e789134e3cd021ec21
Carol D. Toro <toroc@oregonstate.edu>
2016-07-05 12:34:37 -0700

    Only moving the parser file

--- htmlParser/test_parse.py
+++
@@ -1,9 +0,0 @@
-import sys
-from htmlParser import parse_html
-import urllib
-
-result = parse_html(sys.argv[1], sys.argv[2])
-print(result['title'])
-print(result['keyword_found'])
-print(result['urls'])
-print(len(result['urls']))
