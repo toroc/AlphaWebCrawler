0062ab1b04111169859c5970f539293375fdeedd
Carol D. Toro <toroc@oregonstate.edu>
2016-07-05 15:42:47 -0700

    web interface deployed on app engine

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -1,15 +1,10 @@
-import crawlerUI
-from crawlerUI import app
+from crawlerUI import app, views
 import config
 import os, sys
 # Add the parent directory to the path
 # CURRENTDIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 # if CURRENTDIR not in sys.path:
 #     sys.path.append(CURRENTDIR)
-
-
-
-
 
 """Used for running site locally"""
 
