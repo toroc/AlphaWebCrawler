137162cbb5b60ee5e50ff0089e1251582d114885
Carol D. Toro <toroc@oregonstate.edu>
2016-07-19 14:26:34 -0700

    Added app engine files for crawler

---
+++ Crawler/main.py
@@ -0,0 +1,18 @@
+from crawler import app, views
+import config
+import os, sys
+
+#rootdir = os.path.dirname(os.path.abspath(__file__))
+#lib = os.path.join(rootdir, 'lib')
+#sys.path.append(lib)
+#sys.path.insert(0,'lib')
+
+# Add the parent directory to the path
+# CURRENTDIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+# if CURRENTDIR not in sys.path:
+#     sys.path.append(CURRENTDIR)
+
+"""Used for running site locally"""
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=8080, debug=True)
